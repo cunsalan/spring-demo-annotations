42aabb586be3497e83b972e0db197c9bb19c2965
CANUNS <CANUNS@UNSALANCA.intertech.denizbank.com>
2019-03-01 13:38:21 +0300

    log level update

--- spring-demo-annotations/src/com/unsalan/springdemo/SwimJavaConfigDemoApp.java
+++ spring-demo-annotations/src/com/unsalan/springdemo/SwimJavaConfigDemoApp.java
@@ -1,17 +1,20 @@
 package com.unsalan.springdemo;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
 public class SwimJavaConfigDemoApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SportConfig.class);
+		AnnotationConfigApplicationContext logContext = new AnnotationConfigApplicationContext(MyLoggerConfig.class);
+		
+		
 		
 		Coach theCoach = context.getBean("swimCoach",Coach.class);
 		
 		System.out.println(theCoach.getDailyWorkOut());
+		
+		System.out.println(theCoach.getDailyFortune().getFortune());
 		
 		context.close();
 
